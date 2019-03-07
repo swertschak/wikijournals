46a4a2b7bfef9e43f830bd9c4d83e47dab58a447
Karsten Krumrück <kkrumrueck@scopeland.de>
2019-03-07 14:12:47 +0100

    Update install script for windows

--- tools/Install_Wikijournals.cmd
+++ tools/Install_Wikijournals.cmd
@@ -12,31 +12,48 @@ cd ..
 
 
 echo Install Semantic Mediawiki
-call composer require mediawiki/semantic-media-wiki "*.*"
+call composer require mediawiki/semantic-media-wiki "3.0.1"
 cd maintenance
 php update.php
 
 echo Install SMW Extensions
 cd ..\extensions
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/SemanticCompoundQueries.git
+
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/SemanticDrilldown.git
-git clone https://git.wikimedia.org/git/mediawiki/extensions/SemanticForms.git
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/SemanticFormsInputs.git
+git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/PageForms
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/SemanticInternalObjects.git
+
 cd ..
-call composer require mediawiki/maps "*"
-call composer require mediawiki/semantic-maps "*"
-call composer require mediawiki/semantic-result-formats "*"
+echo Install Maps
+call composer require mediawiki/maps "7.1.0"
+
+echo Install Semantic Result Formats
+call composer require mediawiki/semantic-result-formats "3.0.0"
+
+echo Install Semantic Compound Queries
+call composer require mediawiki/semantic-compound-queries "2.0.0"
+
 cd extensions
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/AdminLinks.git
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/DataTransfer.git
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/ExternalData.git
-git clone https://git.wikimedia.org/git/mediawiki/extensions/HeaderTabs.git
-git clone https://git.wikimedia.org/git/mediawiki/extensions/MyVariables.git
+git clone https://phabricator.wikimedia.org/diffusion/EHET/extension-headertabs.git
+ren extension-headertabs HeaderTabs
+git clone https://phabricator.wikimedia.org/diffusion/EMYV/extension-myvariables.git
+ren extension-myvariables MyVariables
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/PageSchemas.git
-git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/ReplaceText.git
-git clone https://git.wikimedia.org/git/mediawiki/extensions/Variables.git
+git clone https://phabricator.wikimedia.org/diffusion/EVAR/extension-variables.git
+ren extension-variables Variables
 git clone https://gerrit.wikimedia.org/r/p/mediawiki/extensions/Widgets.git
+
 cd Widgets
 git submodule init
 git submodule update
+
+cd ..
+copy /b LocalSettings.php+configExtensions.txt LocalSettings.php
+cd maintenance
+php update.php
+
+echo Install Wikijournals Structure
+php importDump.php < $PROJECTDIR/wikijournals_structure/wikijournalsStructure.xml
